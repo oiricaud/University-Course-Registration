16805cc095def416b9b14297f1ba4d5e869cea3b
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-12 14:28:12 -0700

    Okay… so the the application allows the users to register and log in successfully. Everything is stored in a database I created.

---
+++ app/src/main/java/com/restuarants/smart/loginregister/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.restuarants.smart.loginregister;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by oscarricaud on 11/12/16.
+ */
+public class RegisterRequest extends StringRequest{
+
+    private static final String REGISTER_REQUEST_URL = "http://www.narped.com/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String username, int age, String password, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("name", name);
+        params.put("username", username);
+        params.put("password", password);
+        params.put("age", age + "");
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
