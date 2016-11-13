d4145895ea2952049d0630471954d14cf20d2f7c
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-12 23:29:02 -0700

    Use these 2 php files to set up the database on phpmyadmin.

---
+++ Login.php
@@ -0,0 +1,26 @@
+<?php
+    $con =  mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userID, $name, $age, $username, $password);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["name"] = $name;
+        $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
+    }
+    
+    echo json_encode($response);
+?>
