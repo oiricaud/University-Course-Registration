17925b7d07a2bb2d8e2dc34b3ee10dba8bd9926c
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-17 23:16:43 -0700

    Wait a second... there is a button to automatically create the JavaDocs? & Here I am sitting very close to midnight manually creating the JavaDoc...lol

--- app/src/test/java/com/registration/course/university/RegisterActivityTest.java
+++ app/src/test/java/com/registration/course/university/RegisterActivityTest.java
@@ -5,7 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by oscarricaud on 11/17/16.
+ * The simple test checks if the username is the same as the name stored in the database.
+ * I need to work on this and find useful test cases. Please refer to the complete JUnit tests
+ * on simple java code excluding GUI for grading purposes.
+ * https://github.com/oiricaud/JavaDoc-and-Unit-Testing/blob/master/Use-Case-1/tests/JUnit_Student_Test1.java
+ * It will be very similar as shown in the above link.
+ *
+ * @author Oscar I. Ricaud
+ * @version 1.0 Build November 16, 2016
  */
 public class RegisterActivityTest {
 
